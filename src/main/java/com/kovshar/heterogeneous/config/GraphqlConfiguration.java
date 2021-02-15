d62b68e0548e6410d6cd184f1746cbb864973502
RuslanKovshar <ruslan.kovshar@gmail.com>
2021-02-15 22:28:41 +0200

    refactor: move bean declaration to configuration class

--- src/main/java/com/kovshar/heterogeneous/config/GraphqlConfiguration.java
+++ src/main/java/com/kovshar/heterogeneous/config/GraphqlConfiguration.java
@@ -1,41 +1,58 @@
 package com.kovshar.heterogeneous.config;
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-import com.kovshar.heterogeneous.model.User;
-import com.kovshar.heterogeneous.service.UserService;
+import com.kovshar.heterogeneous.graphql.BaseGraph;
 import graphql.GraphQL;
-import graphql.scalars.object.JsonScalar;
+import graphql.analysis.MaxQueryComplexityInstrumentation;
+import graphql.analysis.MaxQueryDepthInstrumentation;
+import graphql.execution.batched.BatchedExecutionStrategy;
+import graphql.execution.instrumentation.ChainedInstrumentation;
 import graphql.schema.GraphQLSchema;
 import io.leangen.graphql.GraphQLSchemaGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
+import io.leangen.graphql.metadata.strategy.query.BeanResolverBuilder;
+import io.leangen.graphql.metadata.strategy.query.PublicResolverBuilder;
+import io.leangen.graphql.metadata.strategy.value.jackson.JacksonValueMapperFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class GraphqlConfiguration {
-    @Bean
-    public JsonScalar jsonScalar() {
-        return new JsonScalar();
-    }
-
-    //private final UserService userService;
-/*
-    @Autowired
-    public GraphqlConfiguration(UserService userService) {
-        this.userService = userService;
-    }
+    private final List<BaseGraph> graphList;
 
     @Bean
-    public GraphQLSchema graphQLSchema() {
-        return new GraphQLSchemaGenerator()
-                .withBasePackages("com.kovshar.heterogeneous")
-                .withOperationsFromSingleton(userService)
+    public GraphQLSchemaGenerator graphQLSchemaGenerator() {
+        GraphQLSchemaGenerator graphQLSchemaGenerator = new GraphQLSchemaGenerator()
+                .withResolverBuilders(
+                        new BeanResolverBuilder("com.kovshar.heterogeneous"),
+                        new AnnotatedResolverBuilder(),
+                        new PublicResolverBuilder("com.kovshar.heterogeneous"));
+
+        for (BaseGraph baseGraph : graphList) {
+            graphQLSchemaGenerator = graphQLSchemaGenerator.withOperationsFromSingleton(baseGraph);
+        }
+        return graphQLSchemaGenerator;
+    }
+
+    @Bean
+    public GraphQLSchema schema() {
+        return graphQLSchemaGenerator()
+                .withValueMapperFactory(new JacksonValueMapperFactory())
                 .generate();
     }
 
     @Bean
     public GraphQL graphQL() {
-        return new GraphQL.Builder(graphQLSchema())
+        return GraphQL.newGraphQL(schema())
+                .queryExecutionStrategy(new BatchedExecutionStrategy())
+                .instrumentation(new ChainedInstrumentation(Arrays.asList(
+                        new MaxQueryComplexityInstrumentation(200),
+                        new MaxQueryDepthInstrumentation(20)
+                )))
                 .build();
-    }*/
+    }
 }
--- src/main/java/com/kovshar/heterogeneous/controller/GraphQLController.java
+++ src/main/java/com/kovshar/heterogeneous/controller/GraphQLController.java
@@ -1,59 +1,21 @@
 package com.kovshar.heterogeneous.controller;
 
-import com.kovshar.heterogeneous.graphql.BaseGraph;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import graphql.analysis.MaxQueryComplexityInstrumentation;
-import graphql.analysis.MaxQueryDepthInstrumentation;
-import graphql.execution.batched.BatchedExecutionStrategy;
-import graphql.execution.instrumentation.ChainedInstrumentation;
-import graphql.schema.GraphQLSchema;
-import io.leangen.graphql.GraphQLSchemaGenerator;
-import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
-import io.leangen.graphql.metadata.strategy.query.BeanResolverBuilder;
-import io.leangen.graphql.metadata.strategy.query.PublicResolverBuilder;
-import io.leangen.graphql.metadata.strategy.value.jackson.JacksonValueMapperFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class GraphQLController {
     private final GraphQL graphQL;
-
-    @Autowired
-    public GraphQLController(List<BaseGraph> graphList) {
-        GraphQLSchemaGenerator graphQLSchemaGenerator = new GraphQLSchemaGenerator()
-                .withResolverBuilders(
-                        new BeanResolverBuilder("com.kovshar.heterogeneous"),
-                        // Resolve by annotations.
-                        new AnnotatedResolverBuilder(),
-                        // Resolve public methods inside root package.
-                        new PublicResolverBuilder("com.kovshar.heterogeneous"));
-
-        for (BaseGraph baseGraph : graphList) {
-            graphQLSchemaGenerator = graphQLSchemaGenerator.withOperationsFromSingleton(baseGraph);
-        }
-        GraphQLSchema schema = graphQLSchemaGenerator
-                .withValueMapperFactory(new JacksonValueMapperFactory())
-                .generate();
-
-        graphQL = GraphQL.newGraphQL(schema)
-                .queryExecutionStrategy(new BatchedExecutionStrategy())
-                .instrumentation(new ChainedInstrumentation(Arrays.asList(
-                        new MaxQueryComplexityInstrumentation(200),
-                        new MaxQueryDepthInstrumentation(20)
-                )))
-                .build();
-    }
 
     @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
